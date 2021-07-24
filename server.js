6d9356d8cd6ebb66f59c72a5c8196e2b76435e8e
270004270young1 <youngminghei@gmail.com>
2021-07-24 11:33:11 +0800

    Initial Author Routes

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const app = express()
 const expressLayouts = require('express-ejs-layouts')
 const mongoose = require('mongoose')
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 
 
 app.set('view engine','ejs')
@@ -16,6 +17,7 @@ app.set('views',__dirname+'/views')
 app.set('layout','layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(express.urlencoded({extended:false}))
 
 
 mongoose.connect(process.env.DATABASE_URL,{
@@ -31,6 +33,7 @@ db.once('open',()=>console.log('Connected to Mongoose'))
 
 
 app.use('/',indexRouter)
+app.use('/authors',authorRouter)
 
 
 app.listen(process.env.PORT || 3000)
